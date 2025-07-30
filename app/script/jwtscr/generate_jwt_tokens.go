5e6a19f7dcea6d2823365eeeba3c2e7921b409d0
Ben Guo <hundunben@gmail.com>
2025-07-30 08:38:29 +0800

    feat(script): Add JWT token generation script and update dependencies
    Added a new Go script `app/script/jwtscr/generate_jwt_tokens.go` for generating JWT tokens.
    Updated `spf13/pflag` dependency in `go.mod` and `go.sum`.

---
+++ app/script/jwtscr/generate_jwt_tokens.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log/slog"
+	"time"
+
+	"github.com/spf13/pflag"
+
+	"github.com/HunDun0Ben/bs_server/app/internal/model/user"
+	"github.com/HunDun0Ben/bs_server/app/pkg/bsjwt"
+	"github.com/HunDun0Ben/bs_server/app/pkg/conf"
+)
+
+var (
+	expire    int
+	refExpire int
+
+	accessTokenDuration  time.Duration
+	refreshTokenDuration time.Duration
+)
+
+var u user.User
+
+func init() {
+	pflag.IntVarP(&expire, "expire", "t", 10, "access token 过期时间, 单位 s")
+	pflag.IntVarP(&refExpire, "refresh token expire", "f", 10, "refresh token 过期时间, 单位 s")
+	pflag.StringVarP(&u.Username, "username", "u", "username", "refresh token 过期时间, 单位 s")
+	pflag.StringSliceVarP(&u.Roles, "roles", "r", []string{"admin", "user"}, "refresh token 过期时间, 单位 s")
+
+}
+
+func main() {
+
+	pflag.Parse()
+	slog.Info("", slog.Int("expire", expire))
+	slog.Info("", slog.Int("refresh token expire", expire))
+	slog.Info("user info", "user", u)
+
+	conf.AppConfig.JWT.Expire = time.Duration(expire * time.Now().Second())
+	atok, rftok, _, _ := bsjwt.GenerateTokenPair(u)
+	slog.Info("access token:", slog.String("", atok))
+	slog.Info("access token:", slog.String("", rftok))
+}
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 )
 
 require (
+	github.com/spf13/pflag v1.0.7
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.4
@@ -60,7 +61,6 @@ require (
 	github.com/sagikazarmark/locafero v0.9.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/cast v1.9.2 // indirect
-	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/tiendc/go-deepcopy v1.6.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
